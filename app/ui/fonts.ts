581ccb5fcacda453596b8be315c2f93d679fa326
SyedSarimMahmood727460 <sarimmahmood78@gmail.com>
2024-08-16 21:55:45 +0500

    initial push

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Import the Inter font with specific weights
+export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+
+// Import the Lusitana font with specific weights
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
